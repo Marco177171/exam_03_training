f0043347039adb26e418458ca98cb2738920ab37
Marco Sebastiani <masebast@e4r3p2.42roma.it>
2022-12-02 19:41:10 +0100

    update

--- mini_paint/mini_paint.c
+++ mini_paint/mini_paint.c
@@ -35,40 +35,41 @@ int ft_check(float x_center, float y_center, float x_index, float y_index, float
 int main(int argc, char *argv[])
 {
 	FILE *file;
-	int height, width, x_index, y_index;
+	int height, width, x_index, y_index, check;
 	char fill, background, id;
-	float x_center, y_center, radius, check;
+	float x_center, y_center, radius;
 	char *result;
 
 	if (argc != 2)
-		return (ft_error("Error: Invalid arguments\n"));
+		return (ft_error("Error: argument\n"));
 	file = fopen(argv[1], "r");
 	if (!file)
-		return (ft_error("Error: file non existent\n"));
-	if (fscanf(file, "%c %d %d", &background, &width, &height) != 3)
-		return (ft_error("Error: first file line faulty\n"));
-	if (width <= 0 || width > 300 || height <= 0 || height > 300)
-		return (ft_error("Error: map too big or negative\n"));
+		return (ft_error("Error: Operation file corrupted\n"));
+	if (fscanf(file, "%c %d %d\n", &background, &width, &height) != 3)
+		return (ft_error("Error: Operation file corrupted\n"));
+	if (width < 1 || width > 300 || height < 1 || height > 300)
+		return (ft_error("Error: Operation file corrupted\n"));
 	result = malloc(sizeof(char) * width * height + height);
 	if (!result)
-		return (ft_error("Error: malloc error\n"));
-	memset(result, background, width * height + height);
-	while (fscanf(file, "%c %f %f %f %c", &id, &x_center, &y_center, &radius, &fill) == 5)
+		return (ft_error("Error: Operation file corrupted\n"));
+	result[width * height + height] = '\0';
+	while (fscanf(file, "%c %f %f %f %c\n", &id, &x_center, &y_center, &radius, &fill) == 5)
 	{
+		if (radius < 0.000000000)
+			return (ft_error("Error: operation file corrupted"));
 		y_index = -1;
-		while (++y_index <= height)
+		while (++y_index < height)
 		{
 			x_index = -1;
 			while (++x_index < width)
 			{
 				check = ft_check(x_center, y_center, (float)x_index, (float)y_index, radius);
 				if (x_index == width - 1)
-					result[x_index * y_index + x_index] = '\n';
-				else if (check == 0 || (check == 1 && id == 'C'))
-				{
-					printf("found\n");
-					result[x_index * y_index + x_index] = fill;
-				}
+					result[y_index * x_index] = '\n';
+				else if (check == 2 || (check == 1 && id == 'C'))
+					result[y_index * x_index] = fill;
+				else
+					result[y_index * x_index] = background;
 			}
 		}
 	}
